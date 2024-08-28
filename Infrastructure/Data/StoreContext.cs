8d753afc0c551afca981e6f9b41034def3e8082d
Shubhankar Puntambekar <dpshubhankar@gmail.com>
2024-08-28 16:00:32 +0530

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,31 @@
+﻿using CORE.Entities;
+using INFRASTRUCTURE.Config;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFRASTRUCTURE.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        { 
+          
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+    }
+
+}
+
+
